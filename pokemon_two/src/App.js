0faef8e1be101f6827dc5a8832397ff81ce63202
Adxell <adxell02@gmail.com>
2021-12-05 16:41:33 -0500

    Update app.js

--- pokemon_two/src/App.js
+++ pokemon_two/src/App.js
@@ -1,8 +1,25 @@
-
+import { useEffect, useState } from "react";
 function App() {
+  const [pokemons, setPokemos]=useState([]);
+  const [load, setLoad]=useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+  const getPokemons= async()=>{
+    const res= await fetch(load);
+    const data = await res.json();
+    setLoad(data.next)
+    console.log(data)
+  }
+  useEffect(()=>{
+    getPokemons();
+  },[])
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="cantainer">
+      <h1>Pokemo</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+
+        </div>
+        <button className="load-more">Load More</button>
+      </div>
     </div>
   );
 }
