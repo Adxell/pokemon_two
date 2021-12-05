fd69b72e3784a512688d50e17cd5104a6c998420
Adxell <adxell02@gmail.com>
2021-12-05 17:34:14 -0500

    Update App.js

--- pokemon_two/src/App.js
+++ pokemon_two/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Pokemon from '../src/components/cardPokemon'
 function App() {
   const [pokemons, setPokemos]=useState([]);
   const [load, setLoad]=useState('https://pokeapi.co/api/v2/pokemon?limit=20');
@@ -6,19 +7,39 @@ function App() {
     const res= await fetch(load);
     const data = await res.json();
     setLoad(data.next)
-    console.log(data)
+    function createPokeObject(results){
+      results.forEach(async (result) => {
+          const res= await fetch(`https://pokeapi.co/api/v2/pokemon/${result.name}`)
+          const data = await res.json()
+          setPokemos(list => [...list, data])
+          console.log(data);
+      });
+    }
+    createPokeObject(data.results)
   }
+  console.log(load)
+
+  
   useEffect(()=>{
     getPokemons();
   },[])
+
   return (
     <div className="cantainer">
       <h1>Pokemo</h1>
       <div className="pokemon-container">
         <div className="all-container">
-
+          {pokemons.map((pokemon, index) => (
+            <Pokemon
+              id={pokemon.id}
+              name={pokemon.name}
+              image={pokemon.sprites.front_default}
+              type={pokemon.types[0].type.name}
+              key={index}
+            />
+          ))}
         </div>
-        <button className="load-more">Load More</button>
+        <button className="load-more" onClick={()=> getPokemons()}>Load More</button>
       </div>
     </div>
   );
