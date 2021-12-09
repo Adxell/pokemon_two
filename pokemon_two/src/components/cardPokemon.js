3a1a3860d213a55c431a3f09e5676e6b5bc9c3de
Adxell <adxell02@gmail.com>
2021-12-09 13:01:01 -0500

    add InView

--- pokemon_two/src/components/cardPokemon.js
+++ pokemon_two/src/components/cardPokemon.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useInView } from 'react-intersection-observer'
 
-const CardPokemon = ({id, name, image, type}) => {
-    const style=`thumb-container ${type}`
-    return (
-        <div className={style}>
-            <div className="detail-wrapper">
-                <h3>{name}</h3>
-                <img src={image} alt={name} />
-                <small>Type: {type}</small>
-            </div>
-        </div>
-    )
-}
+const CardPokemon = ({name, image, type}) => {
+  const style = `thumb-container ${type}`;
+  const { ref, inView} = useInView({
+      root:null,
+      threshold: 0.3,
+  });
+  return (
+    <div className={style}>
+      <div className={`detail-wrapper ${inView}`} ref={ref}>
+        <h3>{name}</h3>
+        <img src={image} alt={name} />
+        <small>Type: {type}</small>
+      </div>
+    </div>
+  );
+};
 
 export default CardPokemon;
